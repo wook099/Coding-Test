a0cb3d819061f48bcabd2bdded79752ad6353301
wook099 <codnrud99@naver.com>
2024-09-21 17:52:16 +0900

    단어 순서 뒤집기

--- src/Study/Main.java
+++ src/Study/Main.java
@@ -1,7 +1,31 @@
 package Study;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Stack;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Dd");
+    public static void main(String[] args) throws Exception {
+
+        var br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String str = br.readLine();
+            String strArr[] = str.split(" ");
+            Stack<String> stack = new Stack<>();
+
+            for (String word : strArr) {
+                stack.push(word);
+            }
+
+            System.out.print("Case #" + (i + 1) + ": ");
+
+            while (!stack.isEmpty()) {
+                System.out.print(stack.pop() + " ");
+            }
+
+            System.out.println();
+        }
     }
 }
