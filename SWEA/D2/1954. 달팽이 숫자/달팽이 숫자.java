2c85cf109b470baa1c19ddc551169490e95b273a
wook099 <89290934+wook099@users.noreply.github.com>
2025-05-14 23:30:53 +0900

    [D2] Title: 달팽이 숫자, Time: 96 ms, Memory: 26,624 KB -BaekjoonHub

---
+++ SWEA/D2/1954. 달팽이 숫자/달팽이 숫자.java
@@ -0,0 +1,89 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+	public static void main(String args[]) throws Exception
+	{
+	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T=Integer.parseInt(br.readLine());
+
+        for(int t=1;t<=T;t++) {
+
+            int N = Integer.parseInt(br.readLine());
+
+            int[][] arr = new int[N][N];
+
+            int[] dx = {0, 1, 0, -1};// 오른쪽 아래 왼쪽 위
+            int[] dy = {1, 0, -1, 0};
+            int x=0;
+            int y=0;
+            int dir=0;
+            int num=1;
+
+
+            for (int i=0;i<N*N;i++){
+
+
+                arr[x][y]=num;
+                num+=1;
+
+                int nx= x+dx[dir];
+                int ny= y+dy[dir];
+
+
+
+                if(nx<0 || nx>=N || ny<0 || ny>=N||arr[nx][ny]!=0){
+                    dir=(dir+1)%4;
+                    nx=x+dx[dir];
+                    ny=y+dy[dir];
+                }
+                x=nx;
+                y=ny;
+
+            }
+            System.out.println("#" + t);
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++)
+                    System.out.print(arr[i][j] + " ");
+
+                System.out.println(" ");
+            }
+
+
+
+
+        }
+	}
+}
