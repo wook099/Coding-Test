d73258a36d180a5498a90f0a8181a5e6ede031be
채우경 <89290934+wook099@users.noreply.github.com>
2026-02-15 15:31:01 +0900

    [Silver I] Title: 미로 탐색, Time: 100 ms, Memory: 14432 KB -BaekjoonHub

--- 백준/Silver/2178. 미로 탐색/미로 탐색.java
+++ 백준/Silver/2178. 미로 탐색/미로 탐색.java
@@ -9,54 +9,57 @@ public class Main {
     static int M;
     static int[][] arr;
     static int cnt = 0;
-    static int[] dx = { -1, 1, 0, 0 };
-    static int[] dy = { 0, 0, -1, 1 };
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st= new StringTokenizer(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
 
-         N= Integer.parseInt(st.nextToken());
-         M = Integer.parseInt(st.nextToken());
-         arr=new int[N+1][M+1];
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        arr = new int[N + 1][M + 1];
 
-         for (int i=1;i<=N;i++){
-             String str =br.readLine();
-             for (int j=1;j<=M;j++){
-                 arr[i][j]=str.charAt(j-1)-'0';
-             }
-         }
+        for (int i = 1; i <= N; i++) {
+            String str = br.readLine();
+            for (int j = 1; j <= M; j++) {
+                arr[i][j] = str.charAt(j - 1) - '0';
+            }
+        }
 
-         bfs(1,1);
+        bfs(1, 1);
 
         System.out.println(arr[N][M]);
     }
 
-    static void bfs(int x,int y){
+    static void bfs(int x, int y) {
 
-        Queue<int[]> queue = new LinkedList<>();
-        queue.add(new int[]{x, y});
+        Queue<int[]> q = new ArrayDeque<>();
+        q.add(new int[]{x, y});
 
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int curX = current[0];
-            int curY = current[1];
+        while (!q.isEmpty()) {
+
+            int[] a = q.poll();
+
+            int cX = a[0];
+            int cY = a[1];
 
             for (int i = 0; i < 4; i++) {
-                int nx = curX + dx[i];
-                int ny = curY + dy[i];
+                int nx = cX + dx[i];
+                int ny = cY + dy[i];
 
-                if (nx < 1 || ny < 1 || nx > N || ny > M) continue;
+                if (nx > N || ny > M || nx < 1 || ny < 1)
+                    continue;
 
-                if (arr[nx][ny] != 1) continue;
+                if (arr[nx][ny] != 1)
+                    continue;
 
-                arr[nx][ny] = arr[curX][curY] + 1;
-                queue.add(new int[]{nx, ny});
+                arr[nx][ny] = arr[cX][cY] + 1;
+                q.add(new int[]{nx, ny});
             }
+
 
         }
     }
-
-
 }
