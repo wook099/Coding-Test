f10a7aff164a891b8a6c079638c513d705ae8a52
채우경 <89290934+wook099@users.noreply.github.com>
2026-02-28 21:34:22 +0900

    [Silver III] Title: N과 M (2), Time: 116 ms, Memory: 15768 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static boolean[] visited;
+    static boolean[][] visite;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        arr= new int[M];// 배열 길이
+        visite = new boolean[N + 1][N+1];// 값에 대한 방문체크
+        visited= new boolean[N+1];
+        sb =new StringBuilder();
+        dfs(0);
+        System.out.println(sb);
+
+    }
+    static void dfs(int depth){
+
+        if (depth==M){
+
+            for (int i=0;i<M;i++){
+                sb.append(arr[i]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+
+        for (int i=1;i<=N;i++) {
+            if (!visited[i]) {
+                if (depth == 0) {
+                    arr[depth] = i;
+                    visited[i] = true;
+                    dfs(depth + 1);
+                    visited[i] = false;
+                }
+
+                else if (arr[depth - 1] < i){
+                    arr[depth] = i;
+                    visited[i]=true;
+                    dfs(depth + 1);
+                    visited[i]=false;
+                }
+
+
+            }
+        }
+    }
+}
