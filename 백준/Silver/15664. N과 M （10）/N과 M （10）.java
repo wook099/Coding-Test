174298cfe5992821446abeb56c6988d6d042309c
채우경 <89290934+wook099@users.noreply.github.com>
2026-03-04 16:52:43 +0900

    [Silver II] Title: N과 M (10), Time: 120 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Silver/15664. N과 M （10）/N과 M （10）.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M;
+    static int[] arr;
+    static int[] result;
+    static boolean[] visited;
+    static StringBuilder sb;
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+
+        st= new StringTokenizer(br.readLine());
+
+        arr= new int[N];// 배열 길이
+        result= new int[N];
+        for (int i=0;i<N;i++){
+            arr[i]=Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+
+        visited= new boolean[N+1];
+        sb =new StringBuilder();
+        dfs(0,0);
+        System.out.println(sb);
+
+    }
+    static void dfs(int depth,int start){
+
+        if (depth==M){
+
+            for (int i=0;i<M;i++){
+                sb.append(result[i]+" ");
+            }
+            sb.append("\n");
+            return;
+        }
+        int pre = 0;
+
+        for (int i=start;i<N;i++){
+            if(!visited[i]&&pre!=arr[i]){
+                visited[i]=true;
+                result[depth]=arr[i];
+                pre = arr[i];
+                dfs(depth+1,i+1);
+                visited[i]=false;
+            }
+
+
+        }
+    }
+}
