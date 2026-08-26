45c064514f2169d8fe76a45172498afb6a9fe99d
채우경 <89290934+wook099@users.noreply.github.com>
2026-08-26 16:45:35 +0900

    [D4] Title: 러시아 국기 같은 깃발, Time: 164 ms, Memory: 29,908 KB -BaekjoonHub

---
+++ SWEA/D4/4613. 러시아 국기 같은 깃발/러시아 국기 같은 깃발.java
@@ -0,0 +1,100 @@
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 기본 제공코드는 임의 수정해도 관계 없습니다. 단, 입출력 포맷 주의
+// 아래 표준 입출력 예제 필요시 참고하세요.
+// 표준 입력 예제
+// int a;
+// double b;
+// char g;
+// String var;
+// long AB;
+// a = sc.nextInt();                           // int 변수 1개 입력받는 예제
+// b = sc.nextDouble();                        // double 변수 1개 입력받는 예제
+// g = sc.nextByte();                          // char 변수 1개 입력받는 예제
+// var = sc.next();                            // 문자열 1개 입력받는 예제
+// AB = sc.nextLong();                         // long 변수 1개 입력받는 예제
+/////////////////////////////////////////////////////////////////////////////////////////////
+// 표준 출력 예제
+// int a = 0;                            
+// double b = 1.0;               
+// char g = 'b';
+// String var = "ABCDEFG";
+// long AB = 12345678901234567L;
+//System.out.println(a);                       // int 변수 1개 출력하는 예제
+//System.out.println(b); 		       						 // double 변수 1개 출력하는 예제
+//System.out.println(g);		       						 // char 변수 1개 출력하는 예제
+//System.out.println(var);		       				   // 문자열 1개 출력하는 예제
+//System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+
+/*
+   사용하는 클래스명이 Solution 이어야 하므로, 가급적 Solution.java 를 사용할 것을 권장합니다.
+   이러한 상황에서도 동일하게 java Solution 명령으로 프로그램을 수행해볼 수 있습니다.
+ */
+class Solution
+{
+
+    public static void main(String[] args) throws IOException {
+
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 1; t <= T; t++) {
+
+            StringTokenizer st = new StringTokenizer(br.readLine());
+
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            char[][] arr = new char[N][M];
+            for (int i = 0; i < N; i++) {
+                String s = br.readLine();
+                for (int j = 0; j < M; j++) {
+                    arr[i][j] = s.charAt(j);
+                }
+            }
+
+            //W -> B -> R
+            //
+
+            int min=Integer.MAX_VALUE;
+            for (int i=0;i<N-2;i++){//white
+                for (int j=i+1;j<N-1;j++){//blue
+
+                    int cnt=0;
+
+                    for (int w = 0; w <= i; w++) {
+                        for (int m = 0; m < M; m++) {
+                            if (arr[w][m] != 'W')
+                                cnt++;
+                        }
+                    }
+
+                    for (int b = i + 1; b <= j; b++) {
+                        for (int m = 0; m < M; m++)
+                            if (arr[b][m] != 'B')
+                                cnt++;
+                    }
+
+                    for (int r = j + 1; r < N; r++) {
+                        for (int m = 0; m < M; m++)
+                            if (arr[r][m] != 'R')
+                                cnt++;
+                    }
+
+                    if (cnt < min)
+                        min = cnt;
+
+                }
+
+
+            }
+            System.out.println("#"+t+" "+min);
+
+        }
+    }
+}
