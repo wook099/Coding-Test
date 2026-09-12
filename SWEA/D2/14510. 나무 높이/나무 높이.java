ed8d913f0b22345651e0ae5e3d55782cefe01d9f
채우경 <89290934+wook099@users.noreply.github.com>
2026-09-12 17:38:17 +0900

    [D2] Title: 나무 높이, Time: 88 ms, Memory: 26,112 KB -BaekjoonHub

--- SWEA/D2/14510. 나무 높이/나무 높이.java
+++ SWEA/D2/14510. 나무 높이/나무 높이.java
@@ -25,8 +25,6 @@
 //System.out.println(var);		       				   // 문자열 1개 출력하는 예제
 //System.out.println(AB);		       				     // long 변수 1개 출력하는 예제
 /////////////////////////////////////////////////////////////////////////////////////////////
-import java.util.Scanner;
-import java.io.FileInputStream;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
@@ -39,63 +37,63 @@ class Solution
 {
 	public static void main(String args[]) throws Exception
 	{
-		/*
-		   아래의 메소드 호출은 앞으로 표준 입력(키보드) 대신 input.txt 파일로부터 읽어오겠다는 의미의 코드입니다.
-		   여러분이 작성한 코드를 테스트 할 때, 편의를 위해서 input.txt에 입력을 저장한 후,
-		   이 코드를 프로그램의 처음 부분에 추가하면 이후 입력을 수행할 때 표준 입력 대신 파일로부터 입력을 받아올 수 있습니다.
-		   따라서 테스트를 수행할 때에는 아래 주석을 지우고 이 메소드를 사용하셔도 좋습니다.
-		   단, 채점을 위해 코드를 제출하실 때에는 반드시 이 메소드를 지우거나 주석 처리 하셔야 합니다.
-		 */
-		//System.setIn(new FileInputStream("res/input.txt"));
- BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+	
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int T = Integer.parseInt(br.readLine());
-        for(int tc = 1; tc <= T; tc++) {
+
+        for (int t=1; t<=T; t++) {
+
+            int max=0;
+
             int N = Integer.parseInt(br.readLine());
 
-            st = new StringTokenizer(br.readLine());
+            int[] arr = new int[N];
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int i=0;i<N;i++){
 
-            int max = 0;
-            int[] trees = new int[N];
-            for (int i = 0; i < N; i++) {
-                int t = Integer.parseInt(st.nextToken());
-                trees[i] = t;
-
-                max = Math.max(max, t);
+                arr[i]=Integer.parseInt(st.nextToken());
+                max=Math.max(max,arr[i]);
+            }
+            int one=0;
+            int two=0;
+            for (int i=0;i<N;i++){
+                one+=(max-arr[i])%2;
+                two+=(max-arr[i])/2;
             }
 
-            //나무가 자라야 할 높이에서 필요한 1, 2의 개 수 구하기
-            int even = 0, odd = 0;
-            for (int i = 0; i < N; i++) {
-                int diff = max - trees[i];
-
-                if (diff == 0) continue;
-
-                even += diff / 2;// 2몫
-                odd += diff % 2;// 1나머지
+            while (one+1<two){//two 가 더 많은건 절대안됨 1로 나누면 더 효율적 분배가 가능해짐
+                two--;
+                one+=2;
             }
+//            int p=0;
+//            if (one>two+1){//쉬는날도 계산이 필요함 1 1 1 -> 5일 필요
+//
+//                p=(one-two-1);
+//            }
 
-            //2 -> 1로 변경   1 2 6  ,  5 4 .9  -> 4 1
-            if (even > odd) {
-                while (Math.abs(even - odd) > 1) {// 3 3
-                    even--;
-                    odd += 2;
-                }
-            }
+            int result =0;
+            if(one==two)
+                result=two*2;
+            else if (one>two) {
+                result=one*2-1;
+            }else
+                result=two*2;
 
-            int result = 0;
-            if (odd > even) { //1이 많을 경우
-                result = odd * 2 - 1;
 
-            } else if (even > odd) { //2가 많을 경우
-                result = even * 2;
+            System.out.println("#"+t+" "+result);
 
-            } else { //1과 2의 숫자가 같을 경우
-                result = odd + even;
-            }
 
-            System.out.println("#" + tc + " " + result);
+
+            /*
+            * 첫째날 1 둘째날 2
+            * 가장높은 나무의 크기가 되기까지 몇일이 걸리는지
+            * 최소날짜 구하기
+            *
+            * 각 나무마다 필요한 개수를 찾아서 one, two 중에 뭘 줘야하는지 구해야함
+            * 1만 필요한 경우를 분류하기 위함임. 1 2개면 3일 필요 ,3개면 5일필요
+            * */
+
         }
     }
 }
